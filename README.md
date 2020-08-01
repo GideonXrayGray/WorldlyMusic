70bcc927683e683a3193a91ba1c5ee73474d0089
gideon <gideon.r.gray@gmail.com>
2020-08-01 16:41:28 -0400

    Added Userinput

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # WorldlyMusic
  A python script that takes an input, translate though random langaues, and back to english.
-
-version 0.1
+ 
+ We found that translating lyrics though multiple lanagues and back to english returned a fun result so I though I should brush up on my python.
+ 
+ 
+**version 0.1.1**
 
 **Todo:** 
 
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 from googletrans import Translator
 import random
 
-statement ="London Ontario is the better London."
+statement =input("Enter text to translate: ")
 translate = Translator()
 langlist = ("af","sq","ar","be","bg","ca","zh-CN","zh-TW","hr","cs","da","nl","en","eo","et","tl","fi","fr","gl","de","el","iw","hi","hu","is","id","ga","it","ja","ko","la","lv","lt","mk","ms","mt","no","fa","pl","pt","ro","ru","sr","sk","sl","es","sw","sv","th","tr","uk","vi","cy","yi")
 rlang = []
@@ -16,5 +16,5 @@ for key in rlang:
     tstatment = translate.translate(dest=key, text=statement)
     statement = tstatment.text
 
-print(translate.translate(statement).text)
+print("Results: " + translate.translate(statement).text)
 print(rlang)
