032ec0553875c227c8dc54c1b18aebfe1be6ed99
gideon <gideon.r.gray@gmail.com>
2020-08-01 16:33:18 -0400

    Main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import sys
+from googletrans import Translator
+import random
+
+statement ="London Ontario is the better London."
+translate = Translator()
+langlist = ("af","sq","ar","be","bg","ca","zh-CN","zh-TW","hr","cs","da","nl","en","eo","et","tl","fi","fr","gl","de","el","iw","hi","hu","is","id","ga","it","ja","ko","la","lv","lt","mk","ms","mt","no","fa","pl","pt","ro","ru","sr","sk","sl","es","sw","sv","th","tr","uk","vi","cy","yi")
+rlang = []
+print("Original Statment: " + statement)
+
+
+while len(rlang) < 3:
+    choice = random.choice(langlist)
+    rlang.append(choice)
+for key in rlang:
+    tstatment = translate.translate(dest=key, text=statement)
+    statement = tstatment.text
+
+print(translate.translate(statement).text)
+
+
+
+
+
+
+
+
+print(rlang)
